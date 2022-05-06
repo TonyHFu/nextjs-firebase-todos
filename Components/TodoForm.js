d34ecb66a1d8aa15553c5a8aed2e690cbc804ab9
tofutigerz <hfu3@ualberta.ca>
2022-05-06 09:47:54 -0600

    adds snackbar for alert

--- Components/TodoForm.js
+++ Components/TodoForm.js
@@ -1,11 +1,12 @@
 import { Button, TextField } from "@mui/material";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { db } from "../firebase";
+import { TodoContext } from "../TodoContext";
 
 const TodoForm = () => {
 	const [todo, setTodo] = useState({ title: "", detail: "" });
-
+	const { showAlert } = useContext(TodoContext);
 	const onSubmit = async () => {
 		const collectionRef = collection(db, "todos");
 		const docRef = await addDoc(collectionRef, {
@@ -13,8 +14,9 @@ const TodoForm = () => {
 			timestamp: serverTimestamp(),
 		});
 		setTodo({ title: "", detail: "" });
-		alert(`Todo with id ${docRef.id} is added successfully`);
+		showAlert("success", `Todo with id ${docRef.id} is added successfully`);
 	};
+
 	return (
 		<div>
 			<TextField
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,51 @@
-import { Container } from "@mui/material";
+import { Alert, Container, Snackbar } from "@mui/material";
 import Head from "next/head";
 import Image from "next/image";
+import { useState } from "react";
 import TodoForm from "../Components/TodoForm";
 import TodoList from "../Components/TodoList";
 import styles from "../styles/Home.module.css";
+import { TodoContext } from "../TodoContext";
 
 export default function Home() {
+	const [open, setOpen] = useState(false);
+	const [alertType, setAlertType] = useState("success");
+	const [alertMessage, setAlertMessage] = useState("");
+
+	const showAlert = (type, message) => {
+		setAlertType(type);
+		setAlertMessage(message);
+		setOpen(true);
+	};
+
+	const handleClose = (event, reason) => {
+		if (reason === "clickaway") {
+			return;
+		}
+
+		setOpen(false);
+	};
+
 	return (
-		<Container maxWidth="sm">
-			<TodoForm />
-			<TodoList />
-		</Container>
+		<TodoContext.Provider value={{ showAlert }}>
+			<Container maxWidth="sm">
+				<TodoForm />
+				<Snackbar
+					open={open}
+					autoHideDuration={6000}
+					onClose={handleClose}
+					anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+				>
+					<Alert
+						onClose={handleClose}
+						severity={alertType}
+						sx={{ width: "100%" }}
+					>
+						{alertMessage}
+					</Alert>
+				</Snackbar>
+				<TodoList />
+			</Container>
+		</TodoContext.Provider>
 	);
 }
