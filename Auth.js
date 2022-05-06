f8b1bb96e8841d58f471d08f87c50d211d6e3124
tofutigerz <hfu3@ualberta.ca>
2022-05-06 15:04:46 -0600

    allows login with google

---
+++ Auth.js
@@ -0,0 +1,21 @@
+import { createContext, useEffect } from "react";
+import { getAuth } from "firebase/auth";
+
+const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }) => {
+	useEffect(() => {
+		const auth = getAuth();
+		return auth.onIdTokenChanged(async user => {
+			if (!user) {
+				console.log("no user");
+				return;
+			}
+			const token = await user.getIdToken();
+			console.log("token", token);
+			console.log("user", user);
+		});
+	}, []);
+
+	return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+};
--- Components/Login.js
+++ Components/Login.js
@@ -1,8 +1,13 @@
 import { Button, Grid } from "@mui/material";
 import React from "react";
 import GoogleIcon from "@mui/icons-material/Google";
+import { signInWithPopup } from "firebase/auth";
+import { provider, auth } from "../firebase";
 
 const Login = ({ type, color }) => {
+	const loginWithGoogle = () => {
+		signInWithPopup(auth, provider);
+	};
 	return (
 		<Grid
 			container
@@ -12,7 +17,11 @@ const Login = ({ type, color }) => {
 			justifyContent="center"
 			style={{ minHeight: "100vh" }}
 		>
-			<Button variant="contained" startIcon={<GoogleIcon></GoogleIcon>}>
+			<Button
+				variant="contained"
+				startIcon={<GoogleIcon></GoogleIcon>}
+				onClick={loginWithGoogle}
+			>
 				Sign in with Google
 			</Button>
 		</Grid>
